b3d81462eef2464f1b7f2c7f49547803b7b9c6bf
Lynn Huang <lynn.huang@eyesmedia.com.tw>
2020-10-13 15:10:44 +0800

    忘記密碼working

--- AFP_Mall/src/app/shared/modal/forget-modal/forget-modal.component.ts
+++ AFP_Mall/src/app/shared/modal/forget-modal/forget-modal.component.ts
@@ -2,29 +2,47 @@ import { Component, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap';
 import { ModalService } from 'src/app/service/modal.service';
 import { AppService } from 'src/app/app.service';
-import { Model_ShareData } from 'src/app/_models';
+import { Model_ShareData, Request_AFPVerify, Response_AFPVerify } from 'src/app/_models';
 
 @Component({
   selector: 'app-forget-modal',
   templateUrl: './forget-modal.component.html'
 })
 export class ForgetModalComponent implements OnInit {
-  public state: boolean;
   public request: Request_AFPChangePwd = new Request_AFPChangePwd();
+  /** 驗證那個驗證碼 */
+  public verify: Request_AFPVerify = {
+    UserInfo_Code: 0,
+    AFPVerify: ''
+  };
+  /** 認證碼 */
 
   constructor(public bsModalRef: BsModalRef, public modal: ModalService, private appService: AppService) {}
 
-  // 註冊送出
+  /** 忘記密碼驗證 */
   onSubmit() {
-    this.appService.openBlock();
-    this.appService.toApi('AFPAccount', '1106', this.request).subscribe((data: Response_AFPChangePwd) => {
-      this.state = true;
+    this.appService.toApi('AFPAccount', '1102', this.verify).subscribe((data: Response_AFPVerify) => {
       const initialState = {
         UserInfoCode: data.UserInfo_Code,
-        Account: this.request.AFPAccount,
-        Type: 1106
+        verifyCode: data.NoticeLog_CheckCode
       };
-      this.modal.show('vcode', { initialState }, this.bsModalRef);
+      console.log(data);
+    });
+
+    // this.appService.toApi('AFPAccount', '1106', this.request).subscribe((data: Response_AFPChangePwd) => {
+    //   const initialState = {
+    //     UserInfoCode: data.UserInfo_Code,
+    //     Account: this.request.AFPAccount,
+    //     Type: 1106
+    //   };
+    //   this.modal.show('vcode', { initialState }, this.bsModalRef);
+    // });
+  }
+
+  /** 取得驗證碼 */
+  setVcode() {
+    this.appService.toApi('AFPAccount', '1106', this.request).subscribe((data: Response_AFPChangePwd) => {
+      console.log(data);
     });
   }
 
