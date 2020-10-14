0a5c146579a94b9306c3ecccc582273e05afd8e5
Lynn Huang <lynn.huang@eyesmedia.com.tw>
2020-10-14 15:35:17 +0800

    修改文案

--- AFP_Mall/src/app/shared/modal/forget-modal/forget-modal.component.ts
+++ AFP_Mall/src/app/shared/modal/forget-modal/forget-modal.component.ts
@@ -31,10 +31,10 @@ export class ForgetModalComponent implements OnInit {
 
   /** 取得驗證碼 */
   setVcode() {
+    this.appService.openBlock();
     this.appService.toApi('AFPAccount', '1106', this.request).subscribe((data: Response_AFPChangePwd) => {
-      this.appService.openBlock();
       this.verify.UserInfo_Code = data.UserInfo_Code;
-      this.vcodeSeconds = 60;
+      this.vcodeSeconds = 59;
       this.vcodeCount = setInterval(() => {
         if ( this.vcodeSeconds > 0 ) {
           this.vcodeSeconds--;
@@ -47,8 +47,8 @@ export class ForgetModalComponent implements OnInit {
 
   /** 忘記密碼驗證 TODO: 待解決驗證成功後要關閉整個視窗 */
   onSubmit() {
+    this.appService.openBlock();
     this.appService.toApi('AFPAccount', '1102', this.verify).subscribe((data: Response_AFPVerify) => {
-      this.appService.openBlock();
       const initialState = {
         UserInfoCode: data.UserInfo_Code,
         verifyCode: data.NoticeLog_CheckCode
