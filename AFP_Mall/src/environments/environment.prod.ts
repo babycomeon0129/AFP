bd80e12658198640675ef33c51fb8949e278b7af
debbbbby <deb.tsaiyi@eyesmedia.com.tw>
2020-11-19 17:13:23 +0800

    [index.html]移除ngx-bootstrap datepicker的css CDN <link>, 改為在angular.json中styles中加入路徑、加入stylePreprocessorOptions參數; 移除在各environment檔、app.module和app.component中推播相關設置

--- AFP_Mall/src/environments/environment.prod.ts
+++ AFP_Mall/src/environments/environment.prod.ts
@@ -4,15 +4,5 @@ export const environment = {
   FBApiKey: '1143928759336931',
   GoogleApiKey: '260499247538-0gafabuvc5n9d2881ufjgefeo71p51q5.apps.googleusercontent.com',
   cookieDomain: 'mobii.ai',
-  cookieSecure: true,
-  firebase: {
-    apiKey: 'AIzaSyDTocp-Q2WObnsQ0hWf4hP2oVQOvRR8hPs',
-    authDomain: 'afp-consumer-1db32.firebaseapp.com',
-    databaseURL: 'https://afp-consumer-1db32.firebaseio.com',
-    projectId: 'afp-consumer-1db32',
-    storageBucket: 'afp-consumer-1db32.appspot.com',
-    messagingSenderId: '248768981886',
-    appId: '1:248768981886:web:f6e311fbe740cbf9c8e12f',
-    measurementId: 'G-2TC2P0Y18C'
-  }
+  cookieSecure: true
 };
