6a11d6bedd07818339c684bb7e6d19ba88f0d318
Lynn Huang <lynn.huang@eyesmedia.com.tw>
2020-11-26 11:44:37 +0800

    新增刮刮樂扣點提醒

--- AFP_Mall/src/app/views/game-center/game-center.component.ts
+++ AFP_Mall/src/app/views/game-center/game-center.component.ts
@@ -16,11 +16,12 @@ export class GameCenterComponent implements OnInit {
   public gameList: AFP_Game[] = [];
 
   constructor(public appService: AppService, private router: Router, public modal: ModalService, private meta: Meta, private title: Title) {
-    // tslint:disable: max-line-length
     this.title.setTitle('遊戲 - Mobii!');
-    this.meta.updateTag({name : 'description', content: 'Mobii! - 遊戲。這裡會顯示 Mobii! 用戶在Mobii! 平台上可以玩的遊戲。遊戲通常會含優惠券、商城的特惠商品以及意想不到的大禮驚喜！趕快註冊登入成為會員吧！'});
+    this.meta.updateTag({name : 'description',
+                         content: 'Mobii! - 遊戲。這裡會顯示 Mobii! 用戶在Mobii! 平台上可以玩的遊戲。遊戲通常會含優惠券、商城的特惠商品以及意想不到的大禮驚喜！趕快註冊登入成為會員吧！'});
     this.meta.updateTag({content: '遊戲 - Mobii!', property: 'og:title'});
-    this.meta.updateTag({content: 'Mobii! - 遊戲。這裡會顯示 Mobii! 用戶在Mobii! 平台上可以玩的遊戲。遊戲通常會含優惠券、商城的特惠商品以及意想不到的大禮驚喜！趕快註冊登入成為會員吧！', property: 'og:description'});
+    this.meta.updateTag({content: 'Mobii! - 遊戲。這裡會顯示 Mobii! 用戶在Mobii! 平台上可以玩的遊戲。遊戲通常會含優惠券、商城的特惠商品以及意想不到的大禮驚喜！趕快註冊登入成為會員吧！',
+                         property: 'og:description'});
 
     this.appService.openBlock();
     const request: Request_GameIndex = {
@@ -33,10 +34,29 @@ export class GameCenterComponent implements OnInit {
     });
   }
 
-  /** 前往遊戲 */
-  GoGame(GameCode: number) {
+  /** 前往遊戲
+   * @param GameCode 就是GameCode
+   * @param GameType 遊戲類型，1:刮刮樂 2:大轉盤
+   * @param GamePoint 領取扣除點數
+   * @param GameExtName 遊戲外部名稱
+   */
+  GoGame(GameCode: number, GameType: number, GamePoint: number, GameExtName: string): void {
     if (this.appService.loginState === true) {
-      this.router.navigate(['/Game', GameCode], {queryParams: { isApp: this.appService.isApp}});
+      // 如果是刮刮樂，要先跳扣除提醒
+      if ( GameType === 1) {
+        this.modal.confirm({
+          initialState: {
+            title: `重要提醒`,
+            message: `遊玩「${GameExtName}」需要扣除 Mobii! Points ${GamePoint} 點，請確定是否繼續？`
+          }
+        }).subscribe( res => {
+          if (res) {
+            this.router.navigate(['/Game', GameCode], {queryParams: { isApp: this.appService.isApp}});
+          }
+        });
+      } else {
+        this.router.navigate(['/Game', GameCode], {queryParams: { isApp: this.appService.isApp}});
+      }
     } else {
       this.appService.loginPage();
     }
@@ -47,9 +67,10 @@ export class GameCenterComponent implements OnInit {
 
 }
 
-export interface Request_GameIndex extends Model_ShareData {}
 
-export interface Response_GameIndex extends Model_ShareData {
+class Request_GameIndex extends Model_ShareData {}
+
+interface Response_GameIndex extends Model_ShareData {
   ADImg_Top: AFP_ADImg;
   List_Game: AFP_Game[];
 }
