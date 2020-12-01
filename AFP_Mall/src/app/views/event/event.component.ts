af9cb2f78a75778eea3109026239e3395d818426
Lynn Huang <lynn.huang@eyesmedia.com.tw>
2020-12-01 16:54:35 +0800

    修復一元搶購，兌換優惠券後，再點擊使用，會再跳出扣點提示的問題，另外在一元搶購、找優惠、優惠券詳細，新增「按鈕狀態為使用＋可用點數兌換」才跳視窗

--- AFP_Mall/src/app/views/event/event.component.ts
+++ AFP_Mall/src/app/views/event/event.component.ts
@@ -50,6 +50,7 @@ export class EventComponent implements OnInit, DoCheck {
     this.appService.toApi('EC', '1205', request).subscribe((data: Response_ECVoucher) => {
       this.voucherList = data.List_Voucher;
       this.coverImg = data.List_ADImg;
+      console.log(this.voucherList);
     });
   }
 
@@ -57,7 +58,7 @@ export class EventComponent implements OnInit, DoCheck {
    * @param voucher 優惠券詳細
    */
   toVoucher(voucher: AFP_Voucher): void {
-    if (voucher.Voucher_DedPoint > 0) {
+    if (voucher.Voucher_DedPoint > 0 && voucher.Voucher_IsFreq === 1) {
       this.modal.confirm({
         initialState: {
           message: `請確定是否扣除 Mobii! Points ${voucher.Voucher_DedPoint} 點兌換「${voucher.Voucher_ExtName}」？`
