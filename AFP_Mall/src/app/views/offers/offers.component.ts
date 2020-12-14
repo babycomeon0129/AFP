7cc48ae20efd2addea2d13a41a3937c643b6af6d
Lynn Huang <lynn.huang@eyesmedia.com.tw>
2020-12-14 17:38:47 +0800

    FIXED: 未登入狀態下，兌換找優惠/一元搶購的優惠券的按鈕點擊之後，會跳出登入頁

--- AFP_Mall/src/app/views/offers/offers.component.ts
+++ AFP_Mall/src/app/views/offers/offers.component.ts
@@ -106,25 +106,29 @@ export class OffersComponent implements OnInit, DoCheck, OnDestroy {
    * @param voucher 優惠券詳細
    */
   toVoucher(voucher: AFP_Voucher): void {
-    if (voucher.Voucher_DedPoint > 0 && voucher.Voucher_IsFreq === 1) {
-      this.modal.confirm({
-        initialState: {
-          message: `請確定是否扣除 Mobii! Points ${voucher.Voucher_DedPoint} 點兌換「${voucher.Voucher_ExtName}」？`
-        }
-      }).subscribe(res => {
-        if (res) {
-          this.appService.onVoucher(voucher);
-        } else {
-          const initialState = {
-            success: true,
-            type: 1,
-            message: `<div class="no-data"><img src="../../../../img/shopping/payment-failed.png"><p>兌換失敗！</p></div>`
-          };
-          this.modal.show('message', { initialState });
-        }
-      });
+    if ( this.appService.loginState) {
+      if (voucher.Voucher_DedPoint > 0 && voucher.Voucher_IsFreq === 1) {
+        this.modal.confirm({
+          initialState: {
+            message: `請確定是否扣除 Mobii! Points ${voucher.Voucher_DedPoint} 點兌換「${voucher.Voucher_ExtName}」？`
+          }
+        }).subscribe(res => {
+          if (res) {
+            this.appService.onVoucher(voucher);
+          } else {
+            const initialState = {
+              success: true,
+              type: 1,
+              message: `<div class="no-data"><img src="../../../../img/shopping/payment-failed.png"><p>兌換失敗！</p></div>`
+            };
+            this.modal.show('message', { initialState });
+          }
+        });
+      } else {
+        this.appService.onVoucher(voucher);
+      }
     } else {
-      this.appService.onVoucher(voucher);
+      this.appService.loginPage();
     }
   }
 
