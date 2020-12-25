0e2ded208262a6ed7cde36fbcfb783f2bc5c8bf8
debbbbby <deb.tsaiyi@eyesmedia.com.tw>
2020-12-25 10:34:32 +0800

    更改uat的api網址

--- AFP_Mall/src/environments/environment.uat.ts
+++ AFP_Mall/src/environments/environment.uat.ts
@@ -1,7 +1,7 @@
 // 壓測使用
 export const environment = {
   production: false,
-  apiUrl: 'http://18.178.141.59:38086/api/',
+  apiUrl: 'https://afpapi-uat.mobii.ai/api/',
   FBApiKey: '349758176149496',
   GoogleApiKey: '260499247538-ctoucp9t09ufdpgqmd4ac368lfpgmorm.apps.googleusercontent.com',
   cookieDomain: '54.150.124.230',
