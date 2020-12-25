fba344a345f4a46226ab85b3f34143367b799a8b
debbbbby <deb.tsaiyi@eyesmedia.com.tw>
2020-12-25 10:44:21 +0800

    uat的environment加上firebase的設置(跟sit一樣)

--- AFP_Mall/src/environments/environment.uat.ts
+++ AFP_Mall/src/environments/environment.uat.ts
@@ -5,5 +5,15 @@ export const environment = {
   FBApiKey: '349758176149496',
   GoogleApiKey: '260499247538-ctoucp9t09ufdpgqmd4ac368lfpgmorm.apps.googleusercontent.com',
   cookieDomain: '54.150.124.230',
-  cookieSecure: false
+  cookieSecure: false,
+  firebase: {
+    apiKey: 'AIzaSyCjtBdCm4T5SdndyaCGHN-S7LJQaLxftsE',
+    authDomain: 'afp-consumer-sit.firebaseapp.com',
+    databaseURL: 'https://afp-consumer-sit.firebaseio.com',
+    projectId: 'afp-consumer-sit',
+    storageBucket: 'afp-consumer-sit.appspot.com',
+    messagingSenderId: '571296338960',
+    appId: '1:571296338960:web:afd1efd62b8c89a970a695',
+    measurementId: 'G-0DDRXNYEBE'
+  }
 };
