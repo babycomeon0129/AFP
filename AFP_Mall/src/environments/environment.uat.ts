bdf7e2bd3fafe7d9e41ec6bf55cd918c2f481296
Lynn Huang <lynn.huang@eyesmedia.com.tw>
2020-12-28 17:36:14 +0800

    修改：UAT的cookieDomain改為mobii.ai

--- AFP_Mall/src/environments/environment.uat.ts
+++ AFP_Mall/src/environments/environment.uat.ts
@@ -1,10 +1,9 @@
-// 壓測使用
 export const environment = {
   production: false,
   apiUrl: 'https://afpapi-uat.mobii.ai/api/',
   FBApiKey: '349758176149496',
   GoogleApiKey: '260499247538-ctoucp9t09ufdpgqmd4ac368lfpgmorm.apps.googleusercontent.com',
-  cookieDomain: '54.150.124.230',
+  cookieDomain: 'mobii.ai',
   cookieSecure: false,
   firebase: {
     apiKey: 'AIzaSyCjtBdCm4T5SdndyaCGHN-S7LJQaLxftsE',
