434c4f6de18fd6bad11ebb40759e313a83cd4cc6
debbbbby <deb.tsaiyi@eyesmedia.com.tw>
2021-02-18 15:10:02 +0800

    移除疑似routing快取測試路徑

--- AFP_Mall/src/app/modules/travel/travel-routing.module.ts
+++ AFP_Mall/src/app/modules/travel/travel-routing.module.ts
@@ -6,8 +6,7 @@ import { SessionAliveGuard } from 'src/app/shared/guard/session-alive-guard/sess
 import { TravelComponent } from './travel.component';
 
 const routes: Routes = [
-  { path: '', canActivate: [SessionAliveGuard], component: TravelComponent },
-  { path: 'test', canActivate: [SessionAliveGuard], component: TravelComponent } // 測試routing疑似快取現象
+  { path: '', canActivate: [SessionAliveGuard], component: TravelComponent }
 ];
 
 @NgModule({
