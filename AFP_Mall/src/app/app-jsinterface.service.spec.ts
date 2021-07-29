0b6d44fe4d03bb7c5ecb786ed4c503be488450b4
Lynn Huang <>
2021-07-29 11:21:17 +0800

    3201：新增AppJSInterfaceService

---
+++ AFP_Mall/src/app/app-jsinterface.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AppJSInterfaceService } from './app-jsinterface.service';
+
+describe('AppJSInterfaceService', () => {
+  let service: AppJSInterfaceService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AppJSInterfaceService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
