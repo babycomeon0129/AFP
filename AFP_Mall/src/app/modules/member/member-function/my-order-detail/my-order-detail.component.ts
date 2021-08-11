ae4fb14bdc79235b59c22a6a1031b45636057f09
mei.wu <mei.wu@eyesmedia.com.tw>
2021-08-11 16:03:14 +0800

    MOB-3197點選「取貨」需call 原生關閉返回鍵修正

--- AFP_Mall/src/app/modules/member/member-function/my-order-detail/my-order-detail.component.ts
+++ AFP_Mall/src/app/modules/member/member-function/my-order-detail/my-order-detail.component.ts
@@ -90,6 +90,10 @@ export class MyOrderDetailComponent implements OnInit, OnDestroy {
 
   /** 取貨（每五秒確認一次，若持續三分鐘則停止並跳出連線逾時訊息） */
   claimOrder(): void {
+    // 點選「取貨」需call 原生關閉返回鍵(MOB-3197)
+    if (this.appService.isApp !== null) {
+      this.appJSInterfaceService.appShowBackButton(true);
+    }
     // 每5秒問一次API是否已取貨
     this.checkTimer = setInterval(() => {
       const request: Request_MemberCheckStatus = {
@@ -98,8 +102,6 @@ export class MyOrderDetailComponent implements OnInit, OnDestroy {
         QRCode: this.orderInfo.Order_QRCode
       };
       this.appService.toApi('Member', '1516', request).subscribe((data: Response_MemberCheckStatus) => {
-        // 點選「取貨」需call 原生關閉返回鍵(MOB-3197)
-        this.appJSInterfaceService.appShowBackButton(true);
         // 若已取貨(鑑賞日期 !== null)，則跳出訊息，3秒後clearInterval()、回到上一頁
         if (data.AFP_MemberOrder.Order_AppreciationDate !== null) {
           clearInterval(this.checkTimer);
@@ -108,7 +110,6 @@ export class MyOrderDetailComponent implements OnInit, OnDestroy {
           this.orderInfo.OrderState = 3;
           this.orderInfo.Order_AppreciationDate = data.AFP_MemberOrder.Order_AppreciationDate;
           this.layerTrig = 0;
-          this.appJSInterfaceService.appShowBackButton(false);
           this.modal.show('message', { initialState: { success: true, message: '收貨愉快!', showType: 1, note: '提醒您，如有退貨需求，請於商品猶豫期內提出申請。'}});
           return false;
         }
