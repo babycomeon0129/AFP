20acf891c1e029fef8a6f51305605d2d836f2541
mei.wu <mei.wu@eyesmedia.com.tw>
2021-09-10 15:53:50 +0800

    移除verify-mobile-modal循環依賴

--- AFP_Mall/src/app/shared/modal/verify-mobile-modal/verify-mobile-modal.component.ts
+++ AFP_Mall/src/app/shared/modal/verify-mobile-modal/verify-mobile-modal.component.ts
@@ -1,7 +1,8 @@
-import { AppService } from './../../../app.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap';
+import { CookieService } from 'ngx-cookie-service';
+import { environment } from '@env/environment';
 
 @Component({
   selector: 'app-verify-mobile-modal',
@@ -12,18 +13,16 @@ export class VerifyMobileModalComponent implements OnInit {
   /** 是否強制手機驗證 */
   public forceVerify = false;
 
-  constructor(public bsModalRef: BsModalRef, private router: Router, private appservice: AppService) { }
+  constructor(public bsModalRef: BsModalRef, private router: Router, private cookieService: CookieService) { }
 
   ngOnInit() {
   }
 
   closeModal(): void {
+    sessionStorage.clear();
+    this.cookieService.deleteAll('/', environment.cookieDomain, environment.cookieSecure, 'Lax');
     this.bsModalRef.hide();
     this.router.navigate(['/']);
-    this.appservice.onLogout();
-    // if (this.target != null && this.target.replace(/(^s*)|(s*$)/g, '').length !== 0) {
-    //   this.router.navigate([this.target]);
-    // }
   }
 
 }
