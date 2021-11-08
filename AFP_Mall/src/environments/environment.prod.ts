b9d60a390fa0cee3605f33e0f43b52477cc613e7
mei.wu <mei.wu@eyesmedia.com.tw>
2021-11-08 14:53:12 +0800

    設定prod環境

--- AFP_Mall/src/environments/environment.prod.ts
+++ AFP_Mall/src/environments/environment.prod.ts
@@ -1,6 +1,10 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://afpapi.mobii.ai/api/',
+  // apiUrl: 'https://afpapi.mobii.ai/api/',
+  apiUrl: 'https://afpapiv2.mobii.ai/api/',
+  loginUrl: 'https://login.mobii.ai/auth/api/v1/login',
+  tokenUrl: 'https://login.mobii.ai/auth/api/v1/token',
+  modifyUrl: 'https://login.mobii.ai/auth/api/v1/memberModify',
   FBApiKey: '1143928759336931',
   GoogleApiKey: '260499247538-0gafabuvc5n9d2881ufjgefeo71p51q5.apps.googleusercontent.com',
   cookieDomain: 'mobii.ai',
