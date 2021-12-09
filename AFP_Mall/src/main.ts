b462796aa9768b8f3e843fb6d416c5b62010b9f4
Lynn Huang <48780224+babycomeon0129@users.noreply.github.com>
2021-12-09 13:56:37 +0800

    MOB-3937：GTM code 拆分測試站/正式站(FE) 新增index-dev.html，用package.json的script區分

--- AFP_Mall/src/main.ts
+++ AFP_Mall/src/main.ts
@@ -11,13 +11,12 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-enableProdMode();
-var code = (environment.production)? 'GTM-KBZ2VT8' : 'GTM-N22PP3B';
-var gtmScript = document.createElement("script");
-gtmScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\
-})(window,document,'script','dataLayer',${code});`;
-document.head.appendChild(gtmScript);
-platformBrowserDynamic().bootstrapModule(AppModule);
+// var code = (environment.production)? 'GTM-KBZ2VT8' : 'GTM-N22PP3B';
+// var gtmScript = document.createElement("script");
+// gtmScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\
+// new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\
+// j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\
+// 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\
+// })(window,document,'script','dataLayer',${code});`;
+// document.head.appendChild(gtmScript);
+// platformBrowserDynamic().bootstrapModule(AppModule);
