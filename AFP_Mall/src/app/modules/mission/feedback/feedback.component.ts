429234d81f7bc14cc59a6815d2442d8f3dbfdb23
mei.wu <mei.wu@eyesmedia.com.tw>
2021-12-16 18:25:55 +0800

    星星滑鼠移過動畫及選取判斷

--- AFP_Mall/src/app/modules/mission/feedback/feedback.component.ts
+++ AFP_Mall/src/app/modules/mission/feedback/feedback.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feedback.component.scss']
 })
 export class FeedbackComponent implements OnInit {
+  /** 星星陣列 */
+  public star = [];
+  /** 滑鼠移到的星星顆數值 */
+  public starHover: number;
+  /** 選擇的星星數值 */
+  public starSelect: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.star = Array(5).fill(5, 1, 5).map((x, i) => i);
+  }
+
+  /** 星星滑鼠移過動畫及選取
+   * @param starIndex 星星索引(-1未選取)
+   * @param event 滑鼠事件
+   */
+  selectStar(starIndex: number, event: string) {
+    // 星星滑鼠移過選取
+    this.starHover = starIndex;
+    // 當滑鼠移開則未選取
+    if (event === 'mouseout' && starIndex === 0) {
+      this.starHover = -1;
+    }
+    // 選取星星數值
+    if (event === 'click' && starIndex > -1) {
+      if (this.starSelect !== starIndex) {
+        this.starSelect = starIndex;
+      } else {
+        // 釋放選取星星數值
+        this.starSelect = -1;
+        this.starHover = -1;
+      }
+    }
+    console.log(this.starHover);
+  }
 }
