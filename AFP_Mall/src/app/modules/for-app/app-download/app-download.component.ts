c48a15dc17b4c59812930b8f574ad18ee7b281ba
mei.wu <mei.wu@eyesmedia.com.tw>
2022-02-08 10:10:18 +0800

    MOB-4022[PRD_1.3.6]app下載頁面返回上一頁後 首頁置底bar消失

--- AFP_Mall/src/app/modules/for-app/app-download/app-download.component.ts
+++ AFP_Mall/src/app/modules/for-app/app-download/app-download.component.ts
@@ -13,7 +13,6 @@ export class AppDownloadComponent implements OnInit {
   /** 裝置類型 1 安卓 2 ios */
   public DeviceType = 1;
   constructor(public appService: AppService) {
-    this.appService.isApp = 1;
     this.Show();
   }
 
@@ -24,11 +23,13 @@ export class AppDownloadComponent implements OnInit {
     if (navigator.userAgent.match(/android/i)) {
       //  Android
       this.DeviceType = 1;
+      this.appService.isApp = 1;
       this.UrlLink = 'https://play.google.com/store/apps/details?id=com.eyesmedia.afp';
       this.GoDownload();
     } else if (navigator.userAgent.match(/(iphone|ipad|ipod);?/i)) {
       //  IOS
       this.DeviceType = 2;
+      this.appService.isApp = 1;
       this.UrlLink = 'https://itunes.apple.com/app/id1512321552';
       this.GoDownload();
     }
