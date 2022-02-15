7a9ad2154d289a764044c0e4fc9fa8d97cb0b806
mei.wu <mei.wu@eyesmedia.com.tw>
2022-02-15 17:13:48 +0800

    壓版號 1.3.9_0215

--- AFP_Mall/src/environments/environment.sit.ts
+++ AFP_Mall/src/environments/environment.sit.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   version: '1.3.9',
-  versionDate: '2022/02/15 09:54:12',
+  versionDate: '2022/02/15 17:12:12',
   apiUrl: 'https://sit-afpapi.mobii.ai/api/',
   loginUrl: 'https://sit-login.mobii.ai/auth/api/v1/',
   FBApiKey: '349758176149496',
